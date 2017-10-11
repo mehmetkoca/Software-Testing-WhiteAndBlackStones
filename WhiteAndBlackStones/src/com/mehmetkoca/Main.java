ff68705b95bf158a253518b12dfe69a98beca790
Mehmet Koca <mehmetkoca@outlook.com>
2017-10-11 11:04:01 +0300

    non-recursive

--- WhiteAndBlackStones/src/com/mehmetkoca/Main.java
+++ WhiteAndBlackStones/src/com/mehmetkoca/Main.java
@@ -9,14 +9,24 @@ public class Main {
     //sıralama ihtimallerinde elde edildiği görülüyor.
     //Taş sayısının iki önceki ve bir önceki sıralama ihtimallerinin toplamı istenen sayının sıralama ihtimallerini veriyor.
     private static Scanner scanner = new Scanner(System.in);
-    public static int fiboFunc(int stones) {
+    public static double fiboFunc(double stones) {
         if(stones == 1) {return 1;}
         if(stones == 2) {return 2;}
-        return fiboFunc(stones-1) + fiboFunc(stones-2);
+
+        double prev=0d, next=1d, result=0d;
+        for (int i = 0; i < stones; i++) {
+            result=prev+next;
+            prev=next;
+            next=result;
+        }
+        return result;
+
+        //return fiboFunc(stones-1) + fiboFunc(stones-2);
     }
     public static void main(String[] args) {
+        //System.out.println("Taşlar 15 basamaklı " + fiboFunc(15) + " farklı şekilde sıralanabilir.");
         System.out.println("Taş sayısını girin: ");
-        int stones = scanner.nextInt();
+        double stones = scanner.nextInt();
         System.out.println("Taşlar " + stones + " basamaklı " + fiboFunc(stones) + " farklı şekilde sıralanabilir.");
     }
 }
