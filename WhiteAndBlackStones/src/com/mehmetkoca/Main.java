91737f272687237f12819ff92e5cbb3926a0e477
Mehmet Koca <mehmetkoca@outlook.com>
2017-10-17 15:08:03 +0300

    update v2

--- WhiteAndBlackStones/src/com/mehmetkoca/Main.java
+++ WhiteAndBlackStones/src/com/mehmetkoca/Main.java
@@ -1,33 +1,46 @@
 package com.mehmetkoca;
 import java.util.Scanner;
 public class Main {
-    //Taş sayıları ardışık olarak artırıldığında çıkan değerlerin fibonacci serisi şeklinde arttığı görülüyor.
-    //İlk taşın her zaman beyaz olduğunu kabul ediyoruz. Sonraki sıralamalarda ikinci taşı bir siyah ve bir beyaz
-    //olarak sabitliyoruz. beyaz sabitlediğimizde olasılıklardan sabitlenen taşın sütununu çıkardığımızda
-    //bir önceki sıralama ihtimallerinde elde edildiği görülüyor.
-    //İkinci taşı siyah taş olarak sabitlersek (yani bs olarak) ve bu iki sutunu görmeden incelersek iki önceki
-    //sıralama ihtimallerinde elde edildiği görülüyor.
-    //Taş sayısının iki önceki ve bir önceki sıralama ihtimallerinin toplamı istenen sayının sıralama ihtimallerini veriyor.
     private static Scanner scanner = new Scanner(System.in);
-    public static double fiboFunc(double stones) {
+    public static long fiboFunc(long stones) {
         if(stones == 1) {return 1;}
         if(stones == 2) {return 2;}
 
-        double prev=0d, next=1d, result=0d;
+        long prev=0, next=1, result=0;
         for (int i = 0; i < stones; i++) {
             result=prev+next;
             prev=next;
             next=result;
         }
         return result;
+    }
+    public static boolean kontrolEt(long deger){
 
-        //return fiboFunc(stones-1) + fiboFunc(stones-2);
+        if(deger > 0 && deger < 100)
+            return true;
+        else return false;
     }
+
+    public static void bul(long deger){
+        System.out.println("Taşlar " + deger + " basamaklı " + fiboFunc(deger) + " farklı şekilde sıralanabilir.");
+    }
+
+
     public static void main(String[] args) {
-        //System.out.println("Taşlar 15 basamaklı " + fiboFunc(15) + " farklı şekilde sıralanabilir.");
-        System.out.println("Taş sayısını girin: ");
-        double stones = scanner.nextInt();
-        boolean quit = false;
-        System.out.println("Taşlar " + (int)stones + " basamaklı " + (int)fiboFunc(stones) + " farklı şekilde sıralanabilir.");
+        System.out.println("0 ile 100 arasında taş sayısını girin: ");
+        try{
+            int stones = scanner.nextInt();
+            while (!kontrolEt(stones)){
+                System.out.println("0 ile 100 arasında değer girin: ");
+                stones = scanner.nextInt();
+            }
+            bul(stones);
+        }catch (Exception e)
+        {
+            System.out.println("Uygun değerler girilmediği için program sonlandı.");
+        }
+
     }
+
+    
 }
